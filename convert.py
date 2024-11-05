e4f9df6e824298c1079743ce916d4bf8c59f6188
koayck <116639605+koayck@users.noreply.github.com>
2024-11-06 07:42:47 +0800

    separate jobs by specialization (query column) in JobsDataset.csv

--- convert.py
+++ convert.py
@@ -1,15 +1,15 @@
 # extract 2 column from csv, and convert to txt file
 import csv
+import os
 
-# set file name
-fileName = 'AIJobsDataset'
+for file in os.listdir('./data'):
+    fileName = file.split('.')[0]
+    with open(f'./data/{fileName}.csv', 'r', encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-with open(f'./data/{fileName}.csv', 'r', encoding="utf8") as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    with open(f'./txt/{fileName}.txt', 'w', encoding="utf-8") as txt_file:
-        next(csv_reader)  # skip the header
-        for line in csv_reader:
-            txt_file.write(f'{line[2]}: {line[3]}\n') # specify the column to extract
+        with open(f'./txt/{fileName}.txt', 'w', encoding="utf-8") as txt_file:
+            next(csv_reader)  # skip the header
+            for line in csv_reader:
+                txt_file.write(f'{line[2]}: {line[3]}\n') # specify the column to extract
 
 print('Done!')
