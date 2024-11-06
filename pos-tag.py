9a42be541bdd39e5f468e16fab723b99a07b8e41
koayck <116639605+koayck@users.noreply.github.com>
2024-11-06 10:17:26 +0800

    update pos tag so that the output is xx_xx instead of ("xx", 'xx')

--- pos-tag.py
+++ pos-tag.py
@@ -1,9 +1,51 @@
+# import os
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize, sent_tokenize
+# nltk.download('stopwords')
+# nltk.download('punkt_tab')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('averaged_perceptron_tagger_eng')
+
+# stop_words = set(stopwords.words('english'))
+
+# tokenized_texts = []
+# file_name = []
+
+# for txt_file in os.listdir('./txt'):
+#     file_name.append(txt_file.split('.')[0])
+
+#     # tokenize the text inside the text file
+#     with open(f'./txt/{txt_file}', 'r', encoding="utf-8") as txt:
+#         text = txt.read()
+#         tokenized_texts.append(sent_tokenize(text))
+
+# # Process the tokenized texts
+# for idx, tokenized_text in enumerate(tokenized_texts):
+#     # Get the corresponding file name (from the first loop)
+#     current_file_name = file_name[idx]
+    
+#     # Create a new file for each original text file
+#     with open(f'./pos-tagged-txt/{current_file_name}.txt', 'w', encoding="utf-8") as tagged_file:
+#         for sentence in tokenized_text:
+#             # Tokenize sentence into words
+#             wordsList = word_tokenize(sentence)
+
+#             # Remove stop words from the word list
+#             wordsList = [w for w in wordsList if w.lower() not in stop_words]
+
+#             # Apply POS tagging
+#             tagged = nltk.pos_tag(wordsList)
+
+#             # Write the tagged sentence to the file
+#             tagged_file.write(str(tagged) + '\n')  # Each sentence's POS tags in a new line
+
 import os
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 nltk.download('stopwords')
-nltk.download('punkt_tab')
+nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('averaged_perceptron_tagger_eng')
 
@@ -12,10 +54,11 @@ stop_words = set(stopwords.words('english'))
 tokenized_texts = []
 file_name = []
 
+# Process text files in './txt'
 for txt_file in os.listdir('./txt'):
     file_name.append(txt_file.split('.')[0])
 
-    # tokenize the text inside the text file
+    # Tokenize the text inside the text file
     with open(f'./txt/{txt_file}', 'r', encoding="utf-8") as txt:
         text = txt.read()
         tokenized_texts.append(sent_tokenize(text))
@@ -37,5 +80,6 @@ for idx, tokenized_text in enumerate(tokenized_texts):
             # Apply POS tagging
             tagged = nltk.pos_tag(wordsList)
 
-            # Write the tagged sentence to the file
-            tagged_file.write(str(tagged) + '\n')  # Each sentence's POS tags in a new line
+            # Format and write the tagged sentence to the file in the desired format (word_POS)
+            formatted_tags = [f"{word}_{tag}" for word, tag in tagged]  # Join word and tag with "_"
+            tagged_file.write(' '.join(formatted_tags) + '\n')  # Write all words in the sentence in the format: word_POS
